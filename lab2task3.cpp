d856f609d3f26c32476072c44615ddbfff6a7ad4
Tahawahid <muhammadtahawahid1@gmail.com>
2023-05-23 20:03:33 +0500

    Convert the currency to different rate

---
+++ lab2task3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float pkrVal, pkrDollor, pkrDirham, pkrRiyal;
+
+    cout << "Enter Pakistan Rupees: ";
+    cin >> pkrVal;
+
+    pkrDollor = pkrVal * 0.0056;
+    pkrDirham = pkrVal * 0.021;
+    pkrRiyal = pkrVal * 0.021;
+
+    cout << "PKR to USD is: " << pkrDollor << "\n";
+    cout << "PKR to AED is: " << pkrDirham << "\n";
+    cout << "PKR to SAR is: " << pkrRiyal << "\n";
+}
