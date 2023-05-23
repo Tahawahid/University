206d1b4450fb695db0fb699734a23ad524d4cca1
Tahawahid <muhammadtahawahid1@gmail.com>
2023-05-23 20:21:26 +0500

    All lab 1 task

---
+++ lab1/lab1task1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using std::cout;
+
+int main()
+{
+
+    cout << "*\n";
+    cout << "*\t*\n";
+    cout << "*\t*\t*\n";
+    cout << "*\t*\t*\t*\n";
+    cout << "*\t*\t*\t*\t*";
+    cout << "\n\n\n"; // taking new lines
+
+    // second pattren
+
+    cout << "\t\t\t\t*\n";
+    cout << "\t\t\t*\t*\t*\n";
+    cout << "\t\t*\t*\t*\t*\t*\n";
+    cout << "\t*\t*\t*\t*\t*\t*\t*\n";
+    // third pattrn
+
+    cout << "\t\t\t\t\t1\n";
+    cout << "\t\t\t\t2\t2\n";
+    cout << "\t\t\t3\t3\t3\n";
+    cout << "\t\t4\t4\t4\t4\n";
+    cout << "\t5\t5\t5\t5\t5\n";
+    return 0;
+}
---
+++ lab1/lab1task4.cpp
@@ -0,0 +1,36 @@
+// Q) Print the user given Table
+
+#include <iostream>
+using std::cin;
+using std::cout;
+
+int main()
+{
+    int i, Table;
+    cout << "Enter Which Table you want to print: ";
+    cin >> Table;
+
+    i = 1;
+    cout << Table << " x " << i << " = " << Table * i << "\n";
+    i++;
+    cout << Table << " x " << i << " = " << Table * i << "\n";
+    i++;
+    cout << Table << " x " << i << " = " << Table * i << "\n";
+    i++;
+    cout << Table << " x " << i << " = " << Table * i << "\n";
+    i++;
+    cout << Table << " x " << i << " = " << Table * i << "\n";
+    i++;
+    cout << Table << " x " << i << " = " << Table * i << "\n";
+    i++;
+    cout << Table << " x " << i << " = " << Table * i << "\n";
+    i++;
+    cout << Table << " x " << i << " = " << Table * i << "\n";
+    i++;
+    cout << Table << " x " << i << " = " << Table * i << "\n";
+    i++;
+    cout << Table << " x " << i << " = " << Table * i << "\n";
+    i++;
+
+    return 0;
+}
