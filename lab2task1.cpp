fc2a12341313d1c9949b684a0bf26cd5818a7665
Tahawahid <muhammadtahawahid1@gmail.com>
2023-05-23 19:57:36 +0500

    Print the basic pattern

---
+++ lab2task1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using std::cout;
+
+int main()
+{
+    cout << "\t\t\t*\n";
+    cout << "\t\t*\t\t*\n";
+    cout << "\t*\t\t*\t\t*\n";
+    cout << "\t\t*\t\t*\n";
+    cout << "\t\t\t*\n";
+    return 0;
+}
